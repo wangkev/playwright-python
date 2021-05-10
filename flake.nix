7d2b05c694e49def24102ef7b09c746606be7d19
Kevin Wang <1149450+wangkev@users.noreply.github.com>
2021-05-10 10:04:11 -0400

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        defaultPackage = pkgs.python3Packages.buildPythonPackage rec {
+          name = "playwright";
+          src = ./.;
+        };
+      }
+    );
+}
